31e890b73f095cbff1b015f5dc0ad83cec16c497
Ana Bellatus <anabellatus@gmail.com>
2024-08-24 20:51:52 +0700

    add logging interceptor

--- app/src/main/java/com/anabell/words/data/datasource/remote/retrofit/RetrofitFactory.kt
+++ app/src/main/java/com/anabell/words/data/datasource/remote/retrofit/RetrofitFactory.kt
@@ -1,13 +1,24 @@
 package com.anabell.words.data.datasource.remote.retrofit
 
 import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 private fun provideRetrofit(): Retrofit {
     return Retrofit.Builder()
         .baseUrl("https://my-json-server.typicode.com/anabellatus/db/")
+        .client(provideOkhttpClient())
         .addConverterFactory(GsonConverterFactory.create(Gson()))
+        .build()
+}
+
+private fun provideOkhttpClient(): OkHttpClient {
+    return OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BASIC)
+        })
         .build()
 }
 
